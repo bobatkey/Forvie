98331a0180baa7f2c794e0316b121226286028a5
Robert Atkey <Robert.Atkey@cis.strath.ac.uk>
2011-05-08 14:09:15 +0100

    Remove unnecessary compareHelper function

--- src/Data/Type/Eq.hs
+++ src/Data/Type/Eq.hs
@@ -9,7 +9,3 @@ class Eq2 f where
 
 class Eq3 f where
     (====) :: f a b -> f c d -> Maybe (a :=: c, b :=: d)
-
-compareHelper :: Eq a => Maybe (a :=: b, c :=: d) -> a -> b -> Maybe (c :=: d)
-compareHelper Nothing             a b  = Nothing
-compareHelper (Just (Refl, Refl)) a a' = if a == a' then Just Refl else Nothing
