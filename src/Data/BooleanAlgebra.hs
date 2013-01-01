dd6f4e2ad82ca6ce9375292a9ef5261358f4befa
Robert Atkey <bob.atkey@gmail.com>
2013-01-01 18:27:54 +0000

    Update the Data.BooleanAlgebra module
    - Add Conjunctive newtype, treats the conjunctive structure of a
      boolean algebra as a monoid. Supporting 'and' and 'all' functions,
      generalizing the ones in Prelude and in Data.Foldable.
    
    - Add Disjunctive newtype, treats the disjunctive structure of a
      boolean algebra as a monoid. Supporting 'or' and 'any' functions,
      generalizing the ones in Prelude and in Data.Foldable.
    
    - Add a copyright message

--- src/Data/BooleanAlgebra.hs
+++ src/Data/BooleanAlgebra.hs
@@ -1,4 +1,40 @@
-module Data.BooleanAlgebra where
+-- |
+-- Module      :  Data.BooleanAlgebra
+-- Copyright   :  Robert Atkey 2012
+-- License     :  BSD3
+--
+-- Maintainer  :  bob.atkey@gmail.com
+-- Stability   :  experimental
+-- Portability :  unknown
+--
+
+module Data.BooleanAlgebra
+    ( BooleanAlgebra (..)
+
+    , Conjunctive (..)
+    , and
+    , all
+
+    , Disjunctive (..)
+    , or
+    , any
+
+    , prop_or_assoc
+    , prop_or_commutative
+    , prop_and_assoc
+    , prop_and_commutative
+    , prop_distrib1
+    , prop_distrib2
+    , prop_absorb1
+    , prop_absorb2
+    , prop_complement1
+    , prop_complement2
+    )
+    where
+
+import Prelude hiding (and, all, or, any)
+import Data.Monoid (Monoid (..))
+import Data.Foldable (Foldable, foldMap)
 
 class BooleanAlgebra a where
     (.&.)      :: a -> a -> a
@@ -7,12 +43,41 @@ class BooleanAlgebra a where
     one        :: a
     zero       :: a
 
+--------------------------------------------------------------------------------
 instance BooleanAlgebra Bool where
     (.&.)      = (&&)
     (.|.)      = (||)
     complement = not
     one        = True
     zero       = False
+
+--------------------------------------------------------------------------------
+newtype Conjunctive a = Conjunctive { fromConjunctive :: a }
+
+instance BooleanAlgebra a => Monoid (Conjunctive a) where
+    mempty = Conjunctive one
+    mappend (Conjunctive x) (Conjunctive y) = Conjunctive (x .&. y)
+
+and :: (BooleanAlgebra a, Foldable t) => t a -> a
+and = fromConjunctive . foldMap Conjunctive
+
+all :: (BooleanAlgebra b, Foldable t) => (a -> b) -> t a -> b
+all f = fromConjunctive . foldMap (Conjunctive . f)
+
+--------------------------------------------------------------------------------
+newtype Disjunctive a = Disjunctive { fromDisjunctive :: a }
+
+instance BooleanAlgebra a => Monoid (Disjunctive a) where
+    mempty = Disjunctive zero
+    mappend (Disjunctive x) (Disjunctive y) = Disjunctive (x .|. y)
+
+or :: (BooleanAlgebra a, Foldable t) => t a -> a
+or = fromDisjunctive . foldMap Disjunctive
+
+any :: (BooleanAlgebra b, Foldable t) => (a -> b) -> t a -> b
+any f = fromDisjunctive . foldMap (Disjunctive . f)
+
+--------------------------------------------------------------------------------
 
 prop_or_assoc :: (BooleanAlgebra a, Eq a) => a -> a -> a -> Bool
 prop_or_assoc a b c     = a .|. (b .|. c) == (a .|. b) .|. c
